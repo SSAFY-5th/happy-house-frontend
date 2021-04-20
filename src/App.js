afe3523385ac962679a3e9e6bf7d4191dcdf1be9
YongjaeLee <dev.yongjaelee@gmail.com>
2021-04-20 10:38:28 +0900

    feat: scrollTo 메서드로 매물 찾는 화면으로 스크롤하는 버튼을 추가한다
    - smooth 옵션을 줘서 부드럽게 이동하도록 함
    - 애니메이션 효과를 부드럽게 볼 수 있음

--- src/App.js
+++ src/App.js
@@ -14,11 +14,22 @@ export default class App {
       new Section({ $target }),
     ];
     this.render();
+    this.handleClickToSearch();
   }
 
   handleLoading = (loadStatus) => {
     this.isLoading = loadStatus;
     this.loading.setState(this.isLoading);
+  handleClickToSearch = () => {
+    const button = document.querySelector('#toSearch');
+    button.addEventListener('click', () => {
+      window.scrollTo({
+        top: document.getElementById('wrapper').clientHeight - 58,
+        left: 0,
+        behavior: 'smooth',
+      });
+    });
+  };
   }
 
   render = () => {
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,19 @@ export default class Header {
     $target.appendChild(this.headerWrapper);
   }
 
-  render() {
+  render = () => {
     const template = `
         <div class="header-container">
             <div class="header-left">
-                <div class="header-logo">HAPPY HOUSE 🏘</div>
+                <div class="header-logo-text"># 해피하우스 🔥</div>
             </div>
             <div class="header-right">
-                <div class="header-button"><a href="#">매물 찾기</a></div>
+                <div class="header-button"><a id="toSearch" href="#">매물 찾기</a></div>
                 <div class="header-button "><button class="header-button-primary" href="#">로그인</button></div>
             </div>
         </div>
     `;
 
     this.headerWrapper.insertAdjacentHTML('afterbegin', template);
-  }
+  };
 }
